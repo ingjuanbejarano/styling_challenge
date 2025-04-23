eba06ce7fad8ae0278802dda9ace1c311f39c21d
Juan Carlos Bejarano <jcbj1292@gmail.com>
2025-04-23 13:20:25 -0500

    ci cd test 1

--- src/components/firmFactComponent/FirmFact.jsx
+++ src/components/firmFactComponent/FirmFact.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import styles from "./FirmFact.module.scss"
-import FirmFactCard from '../firmFactCardComponent/FirmFactCard';
-import closeIcon from "../../assets/closeIcon.svg"
+import React from "react";
+import styles from "./FirmFact.module.scss";
+import FirmFactCard from "../firmFactCardComponent/FirmFactCard";
+import closeIcon from "../../assets/closeIcon.svg";
 
 const FirmFact = () => {
-	return (
-		<div>
-			<div className={styles.TitleContainer}>
-				<h1>Firm Facts</h1>
-				<img src={closeIcon} alt="close icon" />
-			</div>
-			<div className={styles.cardsContainer}>
-				<FirmFactCard buttonText="This is a two line button that terminates with This is a two line button that terminates with" />
-				<FirmFactCard buttonText="This is a two line button that terminates with" />
-				<FirmFactCard buttonText="This is a one line button This is a one line button This is a one line button" buttonVariant="default" />
-				<FirmFactCard buttonText="This is a two line button that terminates with" />
-				<FirmFactCard buttonText="This is a two line button that terminates with" text={"initial"} variant="border" />
-				<FirmFactCard buttonText="This is a two line button that terminates with" text={"disabled"} variant="border" disabled />
-				<FirmFactCard buttonText="This is a two line button that terminates with" text={"hover"} variant="border" />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className={styles.TitleContainer}>
+        <h1>Firm Facts test CI/CD</h1>
+        <img src={closeIcon} alt="close icon" />
+      </div>
+      <div className={styles.cardsContainer}>
+        <FirmFactCard buttonText="This is a two line button that terminates with This is a two line button that terminates with" />
+        <FirmFactCard buttonText="This is a two line button that terminates with" />
+        <FirmFactCard
+          buttonText="This is a one line button This is a one line button This is a one line button"
+          buttonVariant="default"
+        />
+        <FirmFactCard buttonText="This is a two line button that terminates with" />
+        <FirmFactCard
+          buttonText="This is a two line button that terminates with"
+          text={"initial"}
+          variant="border"
+        />
+        <FirmFactCard
+          buttonText="This is a two line button that terminates with"
+          text={"disabled"}
+          variant="border"
+          disabled
+        />
+        <FirmFactCard
+          buttonText="This is a two line button that terminates with"
+          text={"hover"}
+          variant="border"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FirmFact;
